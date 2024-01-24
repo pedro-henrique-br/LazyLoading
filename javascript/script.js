95d44941d4bf9241c33b5d22b11fbcf9c0c07259
Pedro Henrique Admin <pedrohenrique.admin@capricho.local>
2024-01-24 10:15:52 -0300

    first commit

---
+++ javascript/script.js
@@ -0,0 +1,14 @@
+const images = document.querySelectorAll(".image-container img")
+
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if(!entry.isIntersecting)return
+
+    const image = entry.target
+    image.src = image.getAttribute("data-src")
+  })
+}, {})
+
+images.forEach((image) => {
+  observer.observe(image)
+})
